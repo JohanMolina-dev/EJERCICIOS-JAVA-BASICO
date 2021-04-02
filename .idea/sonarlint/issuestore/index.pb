740930be86eeaaba1e05e42d5db6849eb93de7ab
JohanMolina-dev <jsmolina49@gmail.com>
2021-04-01 23:42:54 -0500

    Strings

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,7 @@ X
 (src/com/johan/molina/NumerosImpYPar.java,1\2\121d7e7c664466df2092fa2c67cdd609db7ad32b
 X
 (src/com/johan/molina/ValidacionZero.java,d\f\df66c9ffcf1358d450a5f2bb2ba5c95af6d52a8e
+S
+#src/com/johan/molina/DiaSemana.java,1\9\198c3fef99c88ca5bd0ba5a3472fa3717db3e26f
+U
+%src/com/johan/molina/ModMensajes.java,3\4\348149d62722d8051e6df0fda6dc021b161368b1
--- src/com/johan/molina/ModMensajes.java
+++ src/com/johan/molina/ModMensajes.java
@@ -1,7 +1,44 @@
 package com.johan.molina;
 
+import java.util.Scanner;
+
 public class ModMensajes {
     public static void main(String[] args) {
-        
+        /*
+        Del texto, “La sonrisa sera la mejor arma contra la tristeza”
+        Reemplaza  todas las a del  String  anterior  por  una e,
+        adicionalmente  concatenar  a  esta  frase  una adicional que ustedes quieran
+        incluir por consola y las muestre luego unidas.
+         */
+
+        aPorE();
+
+        /*
+        Realizar una aplicación de consola, que al ingresar una frase por teclado elimine
+        los espacios que esta contenga.
+         */
+        Scanner scanner = new Scanner(System.in);
+        formatoSinEspacios(scanner);
+
+
+    }
+
+    private static void aPorE() {
+        mensaje("Cambio A por E: ");
+        String frase = "La sonrisa sera la mejor arma contra la tristeza. ";
+        String frase2 = "El poder de la imaginacón nos hace infinitos.";
+        String cambio = frase.replaceAll("a", "e").concat(frase2);
+        mensaje(cambio);
+    }
+
+    private static void formatoSinEspacios(Scanner scanner) {
+        mensaje("Ingrese por favor su nueva Frase :");
+        String frasePorConsola = scanner.nextLine();
+        String sinEspacios = frasePorConsola.replaceAll(" ", "" ).trim();
+        mensaje("Su mensaje sin espacios es : "+ sinEspacios);
+    }
+
+    private static void mensaje(String s){
+        System.out.println(s);
     }
 }
